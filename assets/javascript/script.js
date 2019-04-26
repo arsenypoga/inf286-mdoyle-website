691901f171ee80b61d47dfbbee0d64ee266cc6d7
= <=>
2019-04-26 10:56:32 -0400

    Add the rest of the HTML pages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,13 @@
 console.log("Testing");
+
+function strobe(time) {
+	window.setInterval(() => {
+		while (true) {
+			const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+			body = document.getElementsByTagName("body")[0].style.backgroundColor = color;
+		}
+	}, time)
+
+
+
+}
